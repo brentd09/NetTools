8dcbf8d15a64620f4b5f4e4222d657ed76c96a7a
Brent Denny <brent.denny@ddls.com.au>
2019-12-03 23:10:06 +1000

    fixed issues with NewPCImageTools

--- NewPCImageTools.psm1
+++ NewPCImageTools.psm1
@@ -1,17 +1,18 @@
-#function Get-CurrentSubnetIPInfo {
+function Get-CurrentSubnetIPInfo {
   [CmdletBinding()]
   Param()
   try {
-    $PhysicalAdatpter = Get-NetAdapter -Physical -ErrorAction Stop
+    $PhysicalAdatpter = Get-NetAdapter -Physical -ErrorAction Stop | Where-Object {$_.Status -eq 'up'}
     $Script:IPInfo = Get-NetIPAddress -AddressFamily IPv4 -InterfaceIndex $PhysicalAdatpter.ifIndex -ErrorAction Stop
   }
   Catch {
     try {
-      $HyperVAdatpter = Get-NetAdapter | Where-Object {$_.ifIndex -ne $PhysicalAdatpter.ifIndex} -ErrorAction Stop
+      $HyperVAdatpter = Get-NetAdapter | Where-Object {$_.ifIndex -ne $PhysicalAdatpter.ifIndex} -ErrorAction Stop | Where-Object {$_.Status -eq 'up'}
       $Script:IPInfo = Get-NetIPAddress -AddressFamily IPv4 -InterfaceIndex $HyperVAdatpter.ifIndex -ErrorAction Stop
     }
     catch {Write-Warning 'No Adapter could be found for this computer'}
   }
+  [string[]]$AllIPinSubnet = @()
   $SubnetJump = [math]::Pow(2,(8 - ($IPInfo.PrefixLength % 8)))
   $RevOctets = ($IPInfo.IPAddress -split '\.')[3..0]
   [ipaddress]$RevIPAddress = $RevOctets -join '.' 
@@ -20,10 +21,14 @@
   $DottedRevSubnetMask.Address = $RevSmnInt
   $RevSNMOctets = ($DottedRevSubnetMask.IPAddressToString -split '\.')[3..0]
   [ipaddress]$FwdSnmAddress = $RevSNMOctets -join '.'
-  $AndedResult = $RevSmnInt -band $RevIPAddress.Address
+  [bigint]$AndedResult = $RevSmnInt -band $RevIPAddress.Address
   $FirstValidRevIP = [ipaddress]::New($AndedResult + 1)
   $LastValidRevIP  = [ipaddress]::New($FirstValidRevIP.address + $SubnetJump - 3)
-  foreach ($RevNum in (($AndedResult+1)..($AndedResult+$SubnetJump - 2))){
+  [bigint[]]$Range = @()
+  [bigint]$StartRange = $AndedResult + 1
+  [bigint]$EndRange = $AndedResult + $SubnetJump - 2
+  for ($StartRange;$StartRange -le $EndRange; $StartRange = $StartRange + 1) {$Range += $StartRange}
+  foreach ($RevNum in $Range){
     $TempRevIP = ([ipaddress]::new($RevNum)).IPAddressToString
     [string[]]$AllIPinSubnet += (($TempRevIP -split '\.')[3..0]) -join '.'
   }
@@ -37,4 +42,14 @@
     AllValidIPs = $AllIPinSubnet
   }
   return New-Object -TypeName psobject -Property $Hash
-#}
+}
+
+function Grant-TrustedHostsToAll {
+  Set-Item -Path WSMan:\localhost\client\TrustedHosts -Value '*'
+}
+
+function Open-ClassPSSession {
+  $SubnetInfo = Get-CurrentSubnetIPInfo
+  $Sessions = New-PSSession -ComputerName $SubnetInfo.AllValidIPs -ErrorAction SilentlyContinue
+  return $Sessions
+}
